c473c329d5f661bfc8f7fc64a7d9de760229fee5
chrisisaasccc <ctw20.chris.isaac@corketb.ie>
2024-09-23 16:53:28 +0100

    Add files via upload

---
+++ first turtle code.py
@@ -0,0 +1,38 @@
+#author:chris isaac
+#date:23/09/24
+#turtle
+
+#drawing a sqaure
+  import turtle
+  for i in range(0,4):
+      turtle.forward(100)
+      turtle.left(90)
+    
+#drawing a octagon
+ import turtle    
+ for i in range(0,9):
+    turtle.forward(120)
+    turtle.left(45)
+
+
+#drawing a star
+import turtle
+for in range(0,5):
+    turtle forward(150)    
+    turtle right(144)
+      
+     
+import turtle
+#drawing a pattern
+    for in range(36):  
+    for in range(0,6):  
+        turtle forward(100)  
+        turtle right(60)     
+        turtle right(10)        
+
+
+
+
+
+
+   
